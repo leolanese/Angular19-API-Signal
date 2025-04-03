1305a85d2f929a2874f0885bf2d56124ca39fabd
Leo Lanese <laneseleo@gmail.com>
2025-04-03 20:32:27 +0100

    updated Multiple Http Requests + errors

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,14 @@
-import { httpResource } from '@angular/common/http';
-import { computed, Injectable } from '@angular/core';
+import { HttpErrorResponse, httpResource } from '@angular/common/http';
+import { computed, Injectable, signal } from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class ApiService {
   private readonly baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  postId = signal(1);
+  post = httpResource<any>(() => `${this.baseUrl}/posts/${this.postId()}`)
+  user = httpResource<any>(() => `${this.baseUrl}/users/${this.post.value()?.userId}`)
+  comments = httpResource<any>(() => `${this.baseUrl}/comments?postId=${this.post.value()?.userId}`)
 
   // Using httpResource()
   private itemsResource = httpResource<any>(() => ({
@@ -11,10 +16,13 @@ export class ApiService {
     method: 'GET',
     headers: {
       accept: 'application/json'
-    }
+    },
+    defaultValue: { item: 'empty'}
   }));
 
   // Computed signals for the template
   isLoading = this.itemsResource.isLoading;
   items = computed(() => this.itemsResource.value() ?? []);
+  isError = computed(() => this.itemsResource.error() as HttpErrorResponse);
 } 
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,30 +1,43 @@
+import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ApiService } from './../api.service';
 import { UserCardComponent } from './user-card.component';
 
 @Component({
   selector: 'app-user',
-  imports: [UserCardComponent],
+  imports: [UserCardComponent, JsonPipe],
   template: `
-    <h1>Users</h1>
-
     @if (isLoading()) {
       <p>Loading...</p>
-    } @else if (items().length) {
-      <section class="card-container">
-        @for (item of items(); track item) {
-          <app-user-card [user]="item" />
-        }
-      </section>
+    } @else if (isError()){
+        <div style='color: red'>An error occurred: {{ isError() | json }}</div>
     } @else {
-      <div>No elements found</div>
-    }
+      @if (items().length) {
+        <section class="card-container">
+          @for (item of items(); track item) {
+            <app-user-card [user]="item" />
+          }
+        </section>
+      } @else {
+        <div>No elements found</div>
+      }
+   }
+
+  <pre>post: {{ post.value() | json }}</pre>
+  <pre>user: {{ user.value() | json }}</pre>
+  <pre>comments: {{ comments.value() | json }}</pre>    
   `,
 })
 export class UserComponent {
-  private serviceApi = inject(ApiService);
+  private readonly serviceApi = inject(ApiService);
    
   // Signals to support the template
+  isError = this.serviceApi.isError;
   isLoading = this.serviceApi.isLoading;
   items = this.serviceApi.items;
+
+  post = this.serviceApi.post;
+  user = this.serviceApi.user;
+  comments = this.serviceApi.comments;
+  
 }
